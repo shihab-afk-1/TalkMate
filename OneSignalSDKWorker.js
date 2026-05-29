49307dfb4f2c6f84a47db10a68bab63a45f251be
shihab-afk-1 <mohammadshihab211@gmail.com>
2026-05-30 01:23:28 +0600

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,2 +1,5 @@
 // OneSignal Background Service Worker
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+// PWA Service Worker for Offline Caching
+importScripts("/pwa/service-worker.js");
